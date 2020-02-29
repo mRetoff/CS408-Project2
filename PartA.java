82037d2a68c6a0664a9ec05fddce058ba3f26a67
Matt <mretoff@purude.edu>
2020-02-29 13:08:23 -0500

    Added data structs to store function names and pairs

--- PartA.java
+++ PartA.java
@@ -14,10 +14,16 @@ public class PartA {
 		String file = args[0] + ".callgraph";
 		String temp;
 		int support = 3, confidence = 65; //initially set to default values
-
+		int funcSupport;
+		String[] callgraphNode, functionName;
 		Scanner s;
-//		HashMap<String[2], int> map = new HashMap<String[2], int>();
-		List<Map<String,List<String>>> map = new ArrayList<Map<String,List<String>>>();
+		//Counts the number of time each function is called
+		HashMap<String, Integer> nodeSupport = new HashMap<String, Integer>();
+		//Keeps track of how many times each pair occurs
+		HashMap<String[], Integer> pairSupport = new HashMap<String[], Integer>();
+		//Keeps track of all method call pairs in each function
+		HashMap<String, List<String>> funcPairs = new HashMap<String, List<String>>();
+
 		if (args.length > 1) {
 			support = Integer.parseInt(args[1]);
 			confidence = Integer.parseInt(args[2]);
@@ -26,22 +32,25 @@ public class PartA {
 		try {
 			s = new Scanner(new File(file));
 			while (s.hasNextLine()) {
-				goToNextNode(s);
-				temp = s.nextLine();
-				if (temp.isEmpty()) // If line is empty, jump to next callgraph node
-					continue;
-				
+				temp = goToNextNode(s);
+				if (temp.isEmpty())
+					break;
+				callgraphNode = temp.split("\\s+");
+				functionName = callgraphNode[5].split("\'");
+				funcSupport = Integer.parseInt(callgraphNode[6].substring(6)) - 1; //Ignore call from null function
+				nodeSupport.put(functionName[1], funcSupport);
 			}
 		} catch (FileNotFoundException e) {}
 	}
 
 	// Jumps to next callgraph node
-	public static void goToNextNode(Scanner s) {
-		String temp;
+	public static String goToNextNode(Scanner s) {
+		String temp = "";
 		while(s.hasNextLine()) {
 			temp = s.nextLine();
 			if (!temp.contains("null") && temp.contains("Call graph"))
 				break;
 		}
+		return temp;
 	}
 }
